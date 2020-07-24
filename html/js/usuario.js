a6598d450d7f227b5041c8bbfe8c0bf5d6d51eb3
Weslraf <68024752+Weslraf@users.noreply.github.com>
2020-07-24 15:38:33 -0300

    Add files via upload

---
+++ html/js/usuario.js
@@ -0,0 +1,27 @@
+function carregarUsuario(){
+
+    var usuarioLogado = localStorage.getItem("user");
+    if(!usuarioLogado){
+        window.location="index.html";
+    }else{
+        var usuarioJson = JSON.parse(usuarioLogado);
+        document.getElementById("dados").innerHTML=
+
+        "<h5 id='nomeUsuario'>" + usuarioJson.nome + "</h5>" +
+        "<h5>" + usuarioJson.email + "<br>" + 
+        "Código: " + usuarioJson.racf + "<br></h5>";
+    
+    }
+}
+
+function periodo(){
+    var periodo = document.getElementById("periodo").value;
+    if(periodo == 1){
+        window.location="relatorios.html";
+    }else if (periodo == 2){
+        window.location="relatorios2.html";
+    }else{
+        window.alert("Selecione uma opção!!");
+    }
+
+}
